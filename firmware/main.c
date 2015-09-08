b66b8ebd952b5fee36b08a85b6e7d6bcc2a76d2b
Rasmus Buskov <rbuskov@gmail.com>
2015-09-09 00:13:50 +0200

    Blinky with makefile

--- firmware/main.c
+++ firmware/main.c
@@ -16,10 +16,22 @@
 // NM-102 Analog Chorus/Flanger main program.
 //
 
-int main(void)
-{
-//    while (1) {
-//    }
+#include <avr/io.h>                        /* Defines pins, ports, etc */
+#include <util/delay.h>                     /* Functions to waste time */
 
-    return 0;
+int main(void) {
+  DDRB |= 0b0000001;            /* Data Direction Register B:
+                                   writing a one to the bit
+                                   enables output. */
+
+  while (1) {
+
+    PORTB = 0b00000001;          /* Turn on first LED bit/pin in PORTB */
+    _delay_ms(1000);                                           /* wait */
+
+    PORTB = 0b00000000;          /* Turn off all B pins, including LED */
+    _delay_ms(1000);                                           /* wait */
+
+  }                                                  /* End event loop */
+  return (0);                            /* This line is never reached */
 }
